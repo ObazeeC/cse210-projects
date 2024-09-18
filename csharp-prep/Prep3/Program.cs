c1b7511ebfd65401459c06663c31eacf1cb8a056
obazeec <obazee.esosa@yahoo.com>
2024-09-18 19:06:35 +0200

    Prep3 complited, with  the first task of the stretch Challenge

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string userInput = "";
+        int guessNumber = 0;
+        int magicNumber = 0;
+        int guessCount = 0;
+       
+       // Console.Write("What is the magic number?: ");
+        //  userInput = Console.ReadLine();
+       //  magicNumber = int.Parse(userInput);
+
+          Random randomNumber = new Random();
+          magicNumber = randomNumber.Next(1,100);
+
+        Console.Write("What is your guess?: ");
+          userInput = Console.ReadLine();
+          guessNumber = int.Parse(userInput);
+        
+            if (magicNumber > guessNumber)
+         {
+            Console.WriteLine("Guess Higher");
+         }
+         else if (magicNumber < guessNumber)
+         {
+            Console.WriteLine("Guess Lower");
+         }
+         else
+         {
+            Console.WriteLine("You guessed it!");
+         }
+         while (guessNumber != magicNumber){
+            Console.Write("What is your guess?: ");
+          userInput = Console.ReadLine();
+          guessNumber = int.Parse(userInput);
+        
+            if (magicNumber > guessNumber)
+         {
+            Console.WriteLine("Guess Higher");
+         }
+         else if (magicNumber < guessNumber)
+         {
+            Console.WriteLine("Guess Lower");
+         }
+         else
+         {
+            Console.WriteLine("You guessed it!");
+         }
+         guessCount ++;
+         }
+         Console.WriteLine($"You made {guessCount} attempts");
+        
+        
     }
 }
