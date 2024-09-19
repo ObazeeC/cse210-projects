2477b81889be99992db0de3ce7d068070e4aa3c0
obazeec <obazee.esosa@yahoo.com>
2024-09-19 13:46:53 +0200

    Prep5 complited

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string userInput = "";
+
+        DisplayWelcome();
+
+        Console.Write("Please enter your name:");
+        string name = Console.ReadLine();
+        PromptUserName(name);
+
+        Console.Write("Please enter your favorite number:");
+        userInput = Console.ReadLine();
+        int favoriteNumber = int.Parse(userInput);
+        PromptUserNumber(favoriteNumber); 
+        //Console.WriteLine($"Your favorite number is {favoriteNumber}");
+
+       
+        int square = SquareNumber (favoriteNumber);
+       // Console.WriteLine(square);
+        DisplayResult(name, square);
     }
+     static void DisplayWelcome()
+        {
+             Console.WriteLine("Welcome to the program!");
+        }
+
+     static void PromptUserName(string userName)
+     {
+             Console.WriteLine($"Hello {userName}");
+     }
+
+     static int PromptUserNumber(int number)
+     {
+        return number;
+     }
+
+     static int SquareNumber (int number)
+     {
+        int SqNumber= number*number;
+        return SqNumber;
+     } 
+
+     static void DisplayResult (string name, int squared)
+     {
+       
+        Console.WriteLine($"{name}, the square of your number is {squared} ");
+     }
 }
