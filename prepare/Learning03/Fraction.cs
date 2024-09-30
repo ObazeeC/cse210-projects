8980cbe6b8c8fa1fa3db770c3b63fbc0be867980
obazeec <obazee.esosa@yahoo.com>
2024-09-30 22:35:01 +0200

    Learning o3, activity completed

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Runtime.InteropServices;
+public class Fraction {
+private int _top;
+private int _bottom;
+
+public Fraction () {
+    _top = 1;
+    _bottom = 1;
+}
+
+public Fraction (int number){
+_bottom = 1;
+}
+
+public Fraction (int firstNumber, int secondNumber){
+
+}
+
+public int GetNumerator (){
+    return _top;
+}
+
+public int Getdenominator (){
+    return _bottom;
+}
+
+public int SetNumerator (int top){
+_top = top;
+return _top;
+}
+
+public int SetDenominator (int bottom){
+_bottom = bottom;
+return _bottom;
+}
+
+public string GetFractionString(){
+    //string top = _top.ToString();
+    //string bottom = _bottom.ToString();
+    string fraction = $"{_top} / {_bottom}";
+    return fraction;
+}
+
+public double GetDecimalValue(){
+    double top = _top;
+    double bottom = _bottom;
+    return top / bottom;
+}
+}
+
